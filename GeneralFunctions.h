da6fb97cd2e324fe9ec92f797601146417b255ac
Ted <22836197+TedBN@users.noreply.github.com>
2024-06-20 23:27:21 +0300

    Initial upload of files for MostRandomTE

---
+++ GeneralFunctions.h
@@ -0,0 +1,17 @@
+//
+// Created by Ted on 12/05/2024
+//
+
+#ifndef MOSTRANDOM_GENERALFUNCTIONS_H
+#define MOSTRANDOM_GENERALFUNCTIONS_H
+
+std::vector<std::pair<std::string, bool>> promptUser(const std::vector<std::pair<std::string, bool>>&,
+                                    const std::string&, int*);
+
+std::vector<std::pair<std::string, bool>> promptUser(const std::vector<std::pair<std::string, bool>>&,
+       const std::vector<std::pair<std::string, bool>>&, const std::string&, const std::string&, int*);
+
+bool checkForField(int, int);
+int pickRandomNumber(int, int);
+
+#endif //MOSTRANDOM_GENERALFUNCTIONS_H
---
+++ ListFunctions.h
@@ -0,0 +1,32 @@
+//
+// Created by Ted on 11/05/2024
+//
+
+#ifndef MOSTRANDOM_LISTFUNCTIONS_H
+#define MOSTRANDOM_LISTFUNCTIONS_H
+
+#include <unordered_map>
+
+std::vector<std::pair<std::string, bool>> populateRacesCustomizable(int*, int*, int*, int*, int*, int*, int*,
+                                          int*, int*, int*, int*, int*, int*, int*, int*);
+
+std::unordered_map<std::string, int> createRaceMap();
+std::unordered_map<int, float> createDistanceMap();
+std::unordered_map<int, float> createTimeMap();
+std::vector<std::pair<std::string, std::string>> getListOfChildrenFieldUpdatesForSpeedtraps(int);
+std::vector<std::array<std::string, 4>> getSpeedtrapCoordinatesForSprints(int);
+std::vector<std::string> getListOfRaceSpeedtraps(int);
+std::vector<std::array<float, 4>> getFreeRoamZoneCoordinates();
+std::vector<std::array<float, 4>> getFreeRoamZoneRespawnCoordinates();
+std::vector<std::string> getListOfFreeRoamZones();
+std::array<std::string, 41> getListOfRegularOpponents();
+std::array<std::string, 18> getListOfScriptedOpponents();
+std::array<std::string, 6> getListOfStartingOpponents();
+std::array<std::string, 73> getListOfPlayerCars();
+std::array<std::string, 35> getListOfAICars();
+std::array<std::string, 69> getListOfEngineSounds();
+std::array<std::string, 46> getListOfCars();
+std::array<std::string, 12> getListOfFMVs();
+std::array<std::string, 43> getListOfMilestoneSpawnPoints();
+
+#endif //MOSTRANDOM_LISTFUNCTIONS_H
